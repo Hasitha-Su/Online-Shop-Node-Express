95b517819ce90df4e673d807184b598467659e2f
Hasitha_IT17132138 <it17132138@my.sliit.lk>
2021-01-29 12:33:02 +0530

    update __dir to rootDir in routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,17 @@
 const express  = require('express')
 
+const rootDir = require('../util/path')
+
+const path = require('path')
+
 const router = express.Router()
 
 // admin/add-product
 router.get('/add-product', (req, res, next) => {
-    res.send("add-post-get")
+    // res.send("add-post-get")
+    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    //change root dir
+    res.sendFile(path.join(rootDir, '../', 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,14 @@
+const path = require('path')
+
 const express  = require('express')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.send("add-post")
+    // res.send("add-post")
+    // res.sendFile('./views/shop.html')
+    /** dirname gives this dir(routes dir) so have to out of this dir with ../ */
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 })
 
 router.post('product', (req, res, next) => {
